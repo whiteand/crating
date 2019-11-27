1d7a46f2af6cb27b8d56c470f853da8c33322013
Andrew.Beletskiy <andrew.beletskiy@adraba.io>
2019-11-27 12:05:10 +0200

    Added safe import

--- src/containers/Import/Import.jsx
+++ src/containers/Import/Import.jsx
@@ -1,26 +1,63 @@
 import React from "react";
 import { Button, message } from "antd";
-import { useDispatch } from "hooks";
+import { keys, assoc } from "ramda";
+import { useDispatch, useSelector } from "hooks";
 import { setState } from "store/actions";
 import { fromStorage } from "store/storageProvider";
 
+const getNextKey = (listName, currentState) => {
+  const match = listName.match(/(.*)\((\d+)\)$/);
+
+  const name = match ? match[1].trim() : listName.trim();
+  let number = Number.parseInt(match ? match[2] : 1);
+
+  while (true) {
+    const newName = `${name} (${number + 1})`;
+
+    if (!currentState[newName]) return newName;
+
+    number++;
+  }
+};
+
 export const Import = () => {
   const dispatch = useDispatch();
+  const currentState = useSelector(state => state);
   const handleImport = React.useCallback(() => {
-    const json = window.prompt(
+    const importStateJson = window.prompt(
       "Скопируйте сюда данные полученные из кнопки експорт"
     );
 
-    if (!json) return;
+    if (!importStateJson) return;
 
     try {
-      const state = fromStorage(json);
+      const importedState = fromStorage(importStateJson);
 
-      dispatch(setState(state));
+      const resState = {
+        ...currentState
+      };
+
+      for (let key of keys(importedState)) {
+        if (!resState[key]) {
+          resState[key] = importedState[key];
+          continue;
+        }
+
+        const newName = window.prompt(
+          `Импортированные данные содержат список который уже присутствует у вас: ${key}. Введите другое имя, если хотите сохранить предыдущий список`,
+          getNextKey(key, resState)
+        );
+
+        if (!newName) continue;
+
+        resState[newName] = assoc('ratingListId', newName, importedState[key]);
+      }
+
+      dispatch(setState(resState));
       message.success("Импорт удался");
     } catch (error) {
       message.error("Импорт не удался");
     }
-  }, [dispatch]);
+  }, [dispatch, currentState]);
   return <Button onClick={handleImport}>Импорт</Button>;
 };
