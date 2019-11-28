887db511798c11630c5de228ae1e0bf874944f8c
Andrew.Beletskiy <andrew.beletskiy@adraba.io>
2019-11-28 15:24:23 +0200

    Added getting of ratings in charts

--- src/containers/Charts/Charts.jsx
+++ src/containers/Charts/Charts.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { keys } from "ramda";
 import { Select, Typography } from "antd";
 import { useSelector } from "hooks";
+import { getSortedRating } from 'selectors'
 import "./Charts.scss";
 
 export const Charts = () => {
   const ratingListsIds = useSelector(keys);
   const [xListId, setXListId] = React.useState(ratingListsIds[0]);
   const [yListId, setYListId] = React.useState(ratingListsIds[0]);
-
+  const xRating = useSelector(getSortedRating(xListId));
+  const yRating = useSelector(getSortedRating(yListId));
   return (
     <div className="charts-container">
       {ratingListsIds.length === 0 ? (
@@ -18,40 +20,45 @@ export const Charts = () => {
           </Typography.Title>
         </div>
       ) : (
-        <div className="input-lists-wrapper">
-          <div className="list-select-wrapper x">
-            <div className="label-wrapper">По горизонтали</div>
-            <div className="select-wrapper">
-              <Select
-                className="select"
-                value={xListId}
-                getPopupContainer={() => document.querySelector(".app")}
-                onChange={setXListId}
-              >
-                {ratingListsIds.map(id => (
-                  <Select.Option value={id} key={id}>
-                    {id}
-                  </Select.Option>
-                ))}
-              </Select>
+        <div className="not-empty-state">
+          <div className="input-lists-wrapper">
+            <div className="list-select-wrapper x">
+              <div className="label-wrapper">По горизонтали</div>
+              <div className="select-wrapper">
+                <Select
+                  className="select"
+                  value={xListId}
+                  getPopupContainer={() => document.querySelector(".app")}
+                  onChange={setXListId}
+                >
+                  {ratingListsIds.map(id => (
+                    <Select.Option value={id} key={id}>
+                      {id}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </div>
+            </div>
+            <div className="list-select-wrapper y">
+              <div className="label-wrapper">По вертикали</div>
+              <div className="select-wrapper">
+                <Select
+                  className="select"
+                  value={yListId}
+                  getPopupContainer={() => document.querySelector(".app")}
+                  onChange={setYListId}
+                >
+                  {ratingListsIds.map(id => (
+                    <Select.Option value={id} key={id}>
+                      {id}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </div>
             </div>
           </div>
-          <div className="list-select-wrapper y">
-            <div className="label-wrapper">По вертикали</div>
-            <div className="select-wrapper">
-              <Select
-                className="select"
-                value={yListId}
-                getPopupContainer={() => document.querySelector(".app")}
-                onChange={setYListId}
-              >
-                {ratingListsIds.map(id => (
-                  <Select.Option value={id} key={id}>
-                    {id}
-                  </Select.Option>
-                ))}
-              </Select>
-            </div>
+          <div className="chart-wrapper">
+            x: {JSON.stringify(xRating)}, y: {JSON.stringify(yRating)}
           </div>
         </div>
       )}
