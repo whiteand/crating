1cfbb1f72fc77fa5b1c1479f76bb062bab137f88
Andrew.Beletskiy <andrew.beletskiy@adraba.io>
2019-11-28 15:20:24 +0200

    Fixed css of charts

--- src/containers/Charts/Charts.jsx
+++ src/containers/Charts/Charts.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { keys } from 'ramda'
-import { Select } from "antd";
+import { keys } from "ramda";
+import { Select, Typography } from "antd";
 import { useSelector } from "hooks";
 import "./Charts.scss";
 
 export const Charts = () => {
   const ratingListsIds = useSelector(keys);
   const [xListId, setXListId] = React.useState(ratingListsIds[0]);
-  const [yListId, setYListId] = React.useState(ratingListsIds[1]);
+  const [yListId, setYListId] = React.useState(ratingListsIds[0]);
 
   return (
     <div className="charts-container">
-      {ratingListsIds.lenght === 0 ? (
-        <div className="empty-state"></div>
+      {ratingListsIds.length === 0 ? (
+        <div className="empty-state">
+          <Typography.Title level={2}>
+            У вас нет ниодного рейтингового списка
+          </Typography.Title>
+        </div>
       ) : (
-        <div clasName="input-lists-wrapper">
+        <div className="input-lists-wrapper">
           <div className="list-select-wrapper x">
             <div className="label-wrapper">По горизонтали</div>
-            <div className="select">
+            <div className="select-wrapper">
               <Select
+                className="select"
                 value={xListId}
                 getPopupContainer={() => document.querySelector(".app")}
                 onChange={setXListId}
@@ -33,8 +38,9 @@ export const Charts = () => {
           </div>
           <div className="list-select-wrapper y">
             <div className="label-wrapper">По вертикали</div>
-            <div className="select">
-            <Select
+            <div className="select-wrapper">
+              <Select
+                className="select"
                 value={yListId}
                 getPopupContainer={() => document.querySelector(".app")}
                 onChange={setYListId}
